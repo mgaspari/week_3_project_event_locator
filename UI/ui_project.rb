3a85911c88ea86915850b619fef945f339641949
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-22 13:08:52 -0400

    Fixed data getting

--- UI/ui_project.rb
+++ UI/ui_project.rb
@@ -1,5 +1,5 @@
 class InterfaceApp
-  attr_accessor :city, :date, :price, :name
+  attr_accessor :city, :date, :price, :name, :data
 
   def user_selection
     puts "1. Favorite
@@ -8,6 +8,10 @@ class InterfaceApp
     4. Exit"
     puts "Please type the associated number for the choices above"
 
+  end
+
+  def present_results
+    
   end
 
   def user_interface_tm
@@ -21,6 +25,10 @@ class InterfaceApp
     puts "Awesome! Around how much are you looking to spend?"
     @price = gets.chomp.tr('$', '')
     #call function to get events here
+    dataI = DataGet.new(@city, @date, @price)
+    @data = dataI.return_events_under_max
+
+
     #call function to output first 5 values
     #asks for the user to input from a selection
 
--- app/models/get_data.rb
+++ app/models/get_data.rb
@@ -3,21 +3,39 @@ require 'json'
 require 'rest-client'
 class DataGet
 
-  attr_accessor :link, :request
+  attr_accessor :link, :request, :price, :selected_events, :final_data
 
-  def initialize(city)
+  def initialize(city, date, price)
     inputCity = city
-    startDate = "2017-08-23T00:00:00Z"
+    startDate = "#{date}T00:00:00Z" #"2017-08-23T00:00:00Z"
+    @price = price
     @link = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=#{inputCity}&startDateTime=#{startDate}&apikey=vZJkJkrliPbqA8RP1qsXrYlAg6YSns8m"
     @request = JSON.parse(RestClient.get(self.link))
     "k"
+    # self.return_events_under_max
+    # self.get_event_info
   end
 
-  def return_events_under_max(user_max)
-    @request["_embedded"]["events"].select do |event|
-     event["priceRanges"][0]["max"] < user_max
+  def get_event_info
+    @final_data = @selected_events.collect do |event|
+      eventName = event["name"]
+      eventUrl = event["url"]
+      eventPrice = "Min - #{event["priceRanges"][0]["min"]} Max -#{event["priceRanges"][0]["max"]}"
+      [eventName, eventUrl, eventPrice]
     end
   end
+#we changed this so that it doesn't take in a value
+  def return_events_under_max
+  @selected_events =  @request["_embedded"]["events"].select do |event|
+
+      event["priceRanges"] && event["priceRanges"][0]["max"] < @price
+    #  event["priceRanges"][0]["max"] < user_max
+     #puts event["priceRanges"]
+    end
+    self.get_event_info
+  end
+
+
 end
 
 binding.pry
