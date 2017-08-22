a174b466dfc5cee59a3f2d9426199d804f0783c9
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-22 09:00:06 -0400

    Master with data file.

--- app/models/get_data.rb
+++ app/models/get_data.rb
@@ -1,12 +1,32 @@
-class Data
+require 'pry'
+require 'json'
+require 'rest-client'
+class DataGet
 
   attr_accessor :link, :request
 
   def initialize
     @link = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=ICMptHAMURNy82YswWCiPVsIENGeTxRl"
     @request = JSON.parse(RestClient.get(self.link))
+    "k"
   end
 
-  
+  def find_match_city(name)
+    #new
+    @request["_embedded"]["events"].select do |event|
+      event["embedded"]["venues"][0]["city"]["name"] == name
+    end
+  end
+
+  def create_events(arrayEvents)
+    # save this for another class
+    # arrayEvents.each do |event|
+    #
+    # end
+  end
 
 end
+
+binding.pry
+
+"k"
